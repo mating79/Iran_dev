a678cd064be1ad9cba46a19e2bde62e3e2cf8f90
unknown <matingivi1379@gmail.com>
2021-02-08 13:49:45 +0330

    Second-commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="d-flex flex-column align-items-center">
       <Header />
-      <Search handleSearchUsers={handleSearchUsers} />
+      <Search />
       <Users />
       <ToastContainer />
     </div>
