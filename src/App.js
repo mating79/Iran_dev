4c5514f768d1b8fb751988a81d0508b62d4cddb1
unknown <matingivi1379@gmail.com>
2021-02-08 16:44:02 +0330

    Third_commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import { ToastContainer } from "react-toastify";
 import Header from "./components/header/Header";
 import Search from "./components/searchSection/Search";
 import Users from "./components/user/Users";
-import SimpleContext from "./simpleContext/SimpleContext";
 
 const App = () => {
-  const context = useContext(SimpleContext);
-  const { handleSearchUsers } = context;
   return (
     <div className="d-flex flex-column align-items-center">
       <Header />
